11390cb05de7e159eae8e456cd5f05573820703b
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2020-01-24 09:35:18 -0700

    Added Survey.handlebars with SurveyJS example, implelemented Open Street Maps API call, modified home page to show example text/location entries, installed Handlebars4.5.0 to resolve own property issues

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,23 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  // Get restaurants
+  app.post("/api/restaurants", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.json(dbExamples);
+    // });
+    console.log(`/api/restaurants: req.body = ${JSON.stringify(req.body)}`);
+
+    // TODO: logic for calculating restaurant to return goes here
+
+    // Just for testing
+    var tmpData = {"name": "R-Name", 
+                  "address": "1111 SomeStreet, city, ST",  
+                  "phone": "111-111-1111"};
+
+    res.json(tmpData);
+  });
+
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
